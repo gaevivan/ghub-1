aa7be07606f5546da3520dd272c309d116e91c1b
Ivan Gaev <6507085-gaevivan@users.noreply.gitlab.com>
2020-09-25 16:53:57 +0300

    правка команд и настроек

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function start() {
             useUnifiedTopology: true,
             useCreateIndex: true
         });
-        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+        app.listen(PORT, () => console.log(`backend on ${PORT}...`));
     } catch (e) {
         console.log("Server Error", e.message);
         process.exit(1);
